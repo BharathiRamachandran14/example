69c989c3f0aa6f70832f61d2a1e46141ccef7b32
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-08-09 11:07:33 +0100

    added Bang for multiples of 7 using func

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,48 @@
-function fizzbuzz(){
+function fizzbuzz() {
     console.log('FizzBuzz!!!!!!!!!!');
     console.log('==================');
+    console.log('Fizz- multiples of three');
+    console.log('Buzz-multiplesof five');
+    console.log('Bang-multiples of seven')
 
-        for(let i=1;i<=100;i++){
+    for (let i = 1; i <= 100; i++) {
 
-            if(i%3===0 && i%5===0){
-                console.log('FizzBuzz');
-            }else if(i%3===0){
-                console.log('Fizz');
-            }else if (i%5===0){
-                console.log('Buzz');
-            }else {
-                console.log(i);
-            }
-        
-        }     
-        
+        let displayName = "";
+
+        if (MultipleOf3(i)) {
+            displayName += 'Fizz';
+        }
+        if (MultipleOf5(i)) {
+            displayName += 'Buzz';
+        }
+        if (Multipleof7(i)) {
+            displayName += 'Bang';
+        }
+        if (!MultipleOf3(i) && !MultipleOf5(i) && !Multipleof7(i)) {
+            displayName += i.toString();
+        }
+
+        console.log(displayName)
+    }
 }
+
+function MultipleOf3(number) {
+    if (number % 3 === 0) {
+        return true;
+    }
+}
+
+function MultipleOf5(number) {
+    if (number % 5 === 0) {
+        return true;
+    }
+}
+
+function Multipleof7(number) {
+    if (number % 7 === 0) {
+        return true;
+    }
+}
+
+
 fizzbuzz();
